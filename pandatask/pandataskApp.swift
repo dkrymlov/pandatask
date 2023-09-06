75ababd8af0f9ec17799310a7eb8a29507a19943
dkrymlov <91798748+dkrymlov@users.noreply.github.com>
2023-09-06 11:59:42 +0300

    Initial Commit

---
+++ pandatask/pandataskApp.swift
@@ -0,0 +1,17 @@
+//
+//  pandataskApp.swift
+//  pandatask
+//
+//  Created by Даниил Крымлов on 06.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct pandataskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
